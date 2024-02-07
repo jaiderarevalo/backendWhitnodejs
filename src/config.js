fe736c4deb40994c7f0c5fdbf97a95b0674a3c57
jaider <104853951+jaiderarevalo@users.noreply.github.com>
2024-02-07 16:29:07 -0500

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,3 +1,3 @@
-export const URL_BD = "mongodb+srv://jaider12:gavJUwSIhaIS3w3e@cluster0.l6q5x4q.mongodb.net/?retryWrites=true&w=majority"
-export const ACESS_Token = "dbc8ab70-2683-11ee-be56-0242ac120002"
-export const PORT = 7000
+export const URL_BD = ""
+export const ACESS_Token = ""
+export const PORT = 4555
